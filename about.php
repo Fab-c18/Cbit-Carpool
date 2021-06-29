7e5cd3b53712109e937cad9bfa2c4be26d28e33a
Fab-c18 <skmaqeel29@gmail.com>
2021-06-29 09:09:00 +0530

    edit2

--- about.php
+++ about.php
@@ -60,7 +60,7 @@
 		<h2>CBIT CAR POOL </h2>
 	</div>
 		<div id="para">
-		<p>Carpooling (also car-sharing, ride-sharing and lift-sharing) is the sharing of car journeys so that more than one person travels in a car, and prevents the need for others to have to drive to a location themselves.</p>
+		<p>Carpooling (also known as car-sharing, ride-sharing and lift-sharing) is the sharing of car journeys so that more than one person travels in a car, and prevents the need for others to have to drive to a location themselves.</p>
 		<br>
 			<p>By having more people using one vehicle, carpooling reduces each person's travel costs such as: fuel costs, tolls, and the stress of driving. Carpooling is also a more environmentally friendly and sustainable way to travel as sharing journeys reduces air pollution, carbon emissions, traffic congestion on the roads, and the need for parking spaces. Authorities often encourage carpooling, especially during periods of high pollution or high fuel prices. Car sharing is a good way to use up the full seating capacity of a car, which would otherwise remain unused if it were just the driver using the car.
 			</p>
